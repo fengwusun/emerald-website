eb6273bedbe5eebd3b91393466b09041ce2d7a51
fengwusun <fengwu.sun@cfa.harvar.edu>
2026-03-07 10:35:13 -0700

    Update redshift submissions

--- zspec_to_local.sh
+++ zspec_to_local.sh
@@ -2,6 +2,7 @@
 cd /data/emerald/emerald
 git pull origin main
 git add data/redshift-submissions.ndjson
+git add science-projects-state.json
 git add ./*.sh
 git commit -m "Update redshift submissions"
 git push origin main
